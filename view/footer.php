2f831bc8421b7ef1cc442198a455628f79549859
timcharlier6 <128478437+timcharlier6@users.noreply.github.com>
2024-01-03 15:55:56 +0100

    app push

---
+++ view/footer.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+    <?php if ($number) { ?>
+        <a href="index.php">Click here to pick another number!</a>
+    <?php } else { ?>
+        🚀
+        🚀
+        🚀
+        🚀
+        🚀
+        🚀
+        🚀
+    <?php } ?>
+</footer>
